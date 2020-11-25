4d3037cead2c33c50c879d3b4a130e0aa9057739
gzl0035 <gzl0035@auburn.edu>
2020-11-24 22:36:02 -0600

    solved 12th problem, 11242020

--- euler12.py
+++ euler12.py
@@ -1,22 +1,36 @@
 import math
 import numpy
 import matplotlib.pyplot as plt
+
 def trigen(num):
     return int(num * (num + 1)/2)
 
+def isint(num):
+    if num == math.floor(num):
+        return True
+    else:
+        return False
+
 def numdiv(num):
     count = 0
-    for i in range(1,num+1):
-        if num / i == math.floor(num /i):
+    middlenum = math.sqrt(num)
+    for i in range(1,math.floor(middlenum)+1):
+        if isint(num / i):
             count += 1
-    return count
+    if isint(middlenum):
+        return count * 2 - 1
+    else:
+        return count * 2
 
-x = numpy.zeros((2,1000))
-for i in range(1,1000):
-    x[1,i-1] = int(trigen(i))
-    x[0,i-1] = int(numdiv(trigen(i)))
+# for i in range(1,1000000):
+#     if numdiv(trigen(i)) >= 500:
+#         print(trigen(i))
+#         break
+print(numdiv(76576500))
+# x = numpy.zeros((2,100000))
+# for i in range(1,100000):
+#     x[1,i-1] = int(trigen(i))
+#     x[0,i-1] = int(numdiv(trigen(i)))
+# plt.plot(x[1],x[0])
 
-
-plt.plot(x[1],x[0])
-
-plt.show()
+# plt.show()
